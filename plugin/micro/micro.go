2b637683f0d8e463b562ff0b549123281f5600c1
Kenneth Allan <ken@norganna.com>
2019-05-02 00:04:23 +1000

    Change interface name to avoid conflict with imports of the same name.

--- plugin/micro/micro.go
+++ plugin/micro/micro.go
@@ -189,7 +189,7 @@ func (g *micro) generateService(file *generator.FileDescriptor, service *pb.Serv
 
 	// Server registration.
 	g.P("func Register", servName, "Handler(s ", serverPkg, ".Server, hdlr ", serverType, ", opts ...", serverPkg, ".HandlerOption) error {")
-	g.P("type ", unexport(servName), " interface {")
+	g.P("type ", unexport(servName), "Interface interface {")
 
 	// generate interface methods
 	for _, method := range service.Method {
@@ -205,7 +205,7 @@ func (g *micro) generateService(file *generator.FileDescriptor, service *pb.Serv
 	}
 	g.P("}")
 	g.P("type ", servName, " struct {")
-	g.P(unexport(servName))
+	g.P(unexport(servName), "Interface")
 	g.P("}")
 	g.P("h := &", unexport(servName), "Handler{hdlr}")
 	g.P("return s.Handle(s.NewHandler(&", servName, "{h}, opts...))")
